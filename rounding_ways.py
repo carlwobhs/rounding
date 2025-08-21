3e79b51bc692f2b632b3ee0ff69cfbf181872e35
carlwobhs <carl.wright@obhs.school.nz>
2025-08-21 12:30:26 +1200

    Rounding methods fixed f string way

--- rounding_ways.py
+++ rounding_ways.py
@@ -1,4 +1,8 @@
 
 
 total = (10.2823 + 7.6991)/ 2.66 
-print(f'This is the total {total:2f}',round(total,2))  #displaying as a float a number
+
+# don't forget :.2 (I forgot the . when I was showing you that's why it didn't work)
+print(f'This is the total {total:.2f}',round(total,2))  #displaying as a float a number
+
+#colon first : then . (dot) then 2f after the dot... easy.
